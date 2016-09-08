a3776e7df340ba0155bf7cd85ff8ce9d42183750
Cristhian Valencia <Cristhian Valencia>
2016-09-08 00:29:35 -0300

    API - Verbos HTTP POST y GET con Express

--- Clase 02/expressjs/index.js
+++ Clase 02/expressjs/index.js
@@ -2,13 +2,28 @@
 let express = require('express');
 let app = express();
 
-function uno (){
-    function dos(){
-        var asd2 = 2;
-    }
-    var mivariable = 1;
-    console.log(mivariable);//si
-    console.log(asd2); //undefined
-}
+app.get('/', (req, res) => {
+  console.log('Esto es la ruta /');
+  res.send('Hello World!');
+});
 
-console.log(mivariable); // mivariable is not defined
+app.post('/', (req, res) => {
+  console.log('hicieron un post 2');
+  res.send('Bien hiciste un post 2!');
+});
+
+app.post('/delay', (req, res) => {
+    setTimeout(()=>{
+        res.send('respuesta con delay');
+    }, 1500);
+});
+
+app.get('/json-data', (req, res) => {
+    res.send({
+        data: 'test'
+    });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
