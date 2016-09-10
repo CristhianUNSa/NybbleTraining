45e6da4230902a5724b1a67d2683339504147235
Cristhian Valencia <valenciacristhian47@gmail.com>
2016-09-10 01:20:28 -0300

    03-properties

--- Clase 03/ejemplos/03-properties/App.js
+++ Clase 03/ejemplos/03-properties/App.js
@@ -4,19 +4,16 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   render() {
     let txt = this.props.txt;
-    let result = this.props.result;
-    return <h1>{txt} - {result}</h1>
+    return <h1>{txt}</h1>
   }
 }
 App.propTypes = {
   txt: React.PropTypes.string,
-  result: React.PropTypes.string,
   cat: React.PropTypes.number.isRequired
 }
 
 App.defaultProps = {
-  txt: 'Texto del componente',
-  result: 'Result text',
+  txt: 'Texto del componente'
 }
 
 ReactDOM.render(
